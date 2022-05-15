2a404b4b58ef1f04e5238536668b31d506464287
Ryan <rmthomas1998@gmail.com>
2022-05-14 22:07:47 -0400

    update overlay blur

--- app/components/MobileMenu/MobileMenu.js
+++ app/components/MobileMenu/MobileMenu.js
@@ -5,51 +5,58 @@ import { HiArrowSmRight } from "react-icons/hi";
 
 const MobileMenu = ({ isActive, setIsActive }) => {
   return (
-    <div className={styles.container} style={{ width: isActive ? 270 : 0 }}>
-      <div className={styles.exitContainer}>
-        <IoMdClose
-          size={26}
-          style={{ cursor: "pointer" }}
-          onClick={() => setIsActive(false)}
-        />
+    <>
+      <div
+        className={styles.overlay}
+        style={{ display: isActive ? "block" : "none" }}
+        onClick={() => setIsActive(false)}
+      ></div>
+      <div className={styles.container} style={{ width: isActive ? 270 : 0 }}>
+        <div className={styles.exitContainer}>
+          <IoMdClose
+            size={26}
+            style={{ cursor: "pointer" }}
+            onClick={() => setIsActive(false)}
+          />
+        </div>
+        <Link href="/">
+          <a className={styles.link} style={{ borderTop: "1px solid #b5c0be" }}>
+            Home
+            <HiArrowSmRight className={styles.arrow} size={20} />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.link}>
+            Product
+            <HiArrowSmRight className={styles.arrow} size={20} />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.link}>
+            Pricing
+            <HiArrowSmRight className={styles.arrow} size={20} />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.link}>
+            Resources
+            <HiArrowSmRight className={styles.arrow} size={20} />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.link}>
+            Log in
+            <HiArrowSmRight className={styles.arrow} size={20} />
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.link}>
+            Get started
+            <HiArrowSmRight className={styles.arrow} size={20} />
+          </a>
+        </Link>
       </div>
-      <Link href="/">
-        <a className={styles.link} style={{ borderTop: "1px solid #b5c0be" }}>
-          Home
-          <HiArrowSmRight className={styles.arrow} size={20} />
-        </a>
-      </Link>
-      <Link href="/">
-        <a className={styles.link}>
-          Product
-          <HiArrowSmRight className={styles.arrow} size={20} />
-        </a>
-      </Link>
-      <Link href="/">
-        <a className={styles.link}>
-          Pricing
-          <HiArrowSmRight className={styles.arrow} size={20} />
-        </a>
-      </Link>
-      <Link href="/">
-        <a className={styles.link}>
-          Resources
-          <HiArrowSmRight className={styles.arrow} size={20} />
-        </a>
-      </Link>
-      <Link href="/">
-        <a className={styles.link}>
-          Log in
-          <HiArrowSmRight className={styles.arrow} size={20} />
-        </a>
-      </Link>
-      <Link href="/">
-        <a className={styles.link}>
-          Get started
-          <HiArrowSmRight className={styles.arrow} size={20} />
-        </a>
-      </Link>
-    </div>
+    </>
   );
 };
 

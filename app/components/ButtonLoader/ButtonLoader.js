c7bfae7d743b5092df4bfa7be4b41cc017527e9b
Ryan <rmthomas1998@gmail.com>
2022-05-18 20:53:58 -0400

    create account form

--- app/components/ButtonLoader/ButtonLoader.js
+++ app/components/ButtonLoader/ButtonLoader.js
@@ -3,9 +3,9 @@ import styles from "./ButtonLoader.module.css";
 const ButtonLoader = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.ballOne}></div>
-      <div className={styles.ballTwo}></div>
-      <div className={styles.ballThree}></div>
+      <div className={styles.outerCircle}></div>
+      <div className={styles.innerCircle}></div>
+      <div className={styles.ball}></div>
     </div>
   );
 };
--- app/components/Signup/SignupForm/SignupForm.js
+++ app/components/Signup/SignupForm/SignupForm.js
@@ -3,63 +3,77 @@ import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 import { HiChevronDown } from "react-icons/hi";
 import { BsCheckLg } from "react-icons/bs";
-import {useRouter} from 'next/router'
-import axios from 'axios';
+import { useRouter } from "next/router";
 import ButtonLoader from "../../ButtonLoader/ButtonLoader";
+import axios from "axios";
 
-const SignupForm = ({accountInfo, setAccountInfo}) => {
-  const { watch, register, handleSubmit, formState: {errors} } = useForm();
+const SignupForm = ({ accountInfo, setAccountInfo }) => {
+  const {
+    watch,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   // set plan choice in number
   const [plan, setPlan] = useState(1);
   // drop down menu for plan options
   const [selectionContainer, setSelectionContainer] = useState(false);
   // to display loading message
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   // plan selected to display in words
   const [selectedPlan, setSelectedPlan] = useState();
   // message if email has been taken
   const [email, setEmail] = useState(false);
-  const [buttonContent, setButtonContent] = useState()
+  // state to update inner button content
+  const [buttonContent, setButtonContent] = useState();
   // router
   const router = useRouter();
 
   useEffect(() => {
-    if (plan == 1) return setButtonContent('Create account');
-    return setButtonContent('Continue to payment')
-  }, [plan])
+    if (plan == 1) return setButtonContent("Create account");
+    return setButtonContent("Continue to payment");
+  }, [plan]);
 
   // function that runs on form submission
-  const submit = async (data) => {
-    if (plan === 1) {
-      
+  const onSubmit = async (data) => {
+    if (plan !== 1) {
+      setAccountInfo({ data, plan: plan });
+    } else {
+      // create free account no need for payment details
+      // make call to backend and then redirect to login page
+      setIsLoading(true);
+      const response = await axios.post("/api/create-customer", {
+        data,
+        plan,
+      });
     }
-  }
+  };
 
   // updates the selected plan
   useEffect(() => {
-    switch(plan) {
+    switch (plan) {
       case 1:
-        setSelectedPlan('Free $0.00 / month');
-        break
+        setSelectedPlan("Free $0.00 / month");
+        break;
       case 2:
-        setSelectedPlan('Standard $8.99 / month');
-        break
+        setSelectedPlan("Standard $8.99 / month");
+        break;
       case 3:
-        setSelectedPlan('Premium $14.99 / month');
-        break
+        setSelectedPlan("Premium $14.99 / month");
+        break;
     }
-  }, [plan])
+  }, [plan]);
 
   // automatically updates plan selection from customer
   // from plans page
   useEffect(() => {
-    if (!router.query.subscription) return
-    setPlan(Number(router.query.subscription))
-  }, [])
+    if (!router.query.subscription) return;
+    setPlan(Number(router.query.subscription));
+  }, []);
 
   return (
-    <div className={styles.wrapper} >
-      <form className={styles.container} onSubmit={handleSubmit(submit)}>
+    <div className={styles.wrapper}>
+      <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
         <p className={styles.title}>Account Information</p>
         <div className={styles.nameContainer}>
           <div
@@ -73,7 +87,12 @@ const SignupForm = ({accountInfo, setAccountInfo}) => {
               className={styles.input}
               {...register("first", { required: true })}
             />
-            <p className={styles.error} style={{opacity: errors.first ? 1 : 0}}>* required field</p>
+            <p
+              className={styles.error}
+              style={{ opacity: errors.first ? 1 : 0 }}
+            >
+              * required field
+            </p>
           </div>
           <div className={styles.inputContainer} style={{ marginRight: 0 }}>
             <span className={watch("last") ? styles.filled : styles.label}>
@@ -84,7 +103,12 @@ const SignupForm = ({accountInfo, setAccountInfo}) => {
               className={styles.input}
               {...register("last", { required: true })}
             />
-            <p className={styles.error} style={{opacity: errors.last ? 1 : 0}}>* required field</p>
+            <p
+              className={styles.error}
+              style={{ opacity: errors.last ? 1 : 0 }}
+            >
+              * required field
+            </p>
           </div>
         </div>
         <div className={styles.inputContainer}>
@@ -96,7 +120,9 @@ const SignupForm = ({accountInfo, setAccountInfo}) => {
             className={styles.input}
             {...register("email", { required: true })}
           />
-          <p className={styles.error} style={{opacity: errors.email ? 1 : 0}}>* required field</p>
+          <p className={styles.error} style={{ opacity: errors.email ? 1 : 0 }}>
+            * required field
+          </p>
         </div>
         <div className={styles.inputContainer}>
           <span className={watch("password") ? styles.filled : styles.label}>
@@ -107,8 +133,23 @@ const SignupForm = ({accountInfo, setAccountInfo}) => {
             className={styles.input}
             {...register("password", { required: true, minLength: 8 })}
           />
-          <p className={styles.error} style={{opacity: errors.password && errors.password?.type !== 'minLength' ? 1 : 0}}>* required field</p>
-          <p className={styles.error} style={{opacity: errors.password?.type === 'minLength' ? 1 : 0}}>* password must be 8 characters</p>
+          <p
+            className={styles.error}
+            style={{
+              opacity:
+                errors.password && errors.password?.type !== "minLength"
+                  ? 1
+                  : 0,
+            }}
+          >
+            * required field
+          </p>
+          <p
+            className={styles.error}
+            style={{ opacity: errors.password?.type === "minLength" ? 1 : 0 }}
+          >
+            * password must be 8 characters
+          </p>
         </div>
         <div
           className={styles.inputContainer}
@@ -118,7 +159,15 @@ const SignupForm = ({accountInfo, setAccountInfo}) => {
           <div className={styles.input} style={{ cursor: "pointer" }}>
             <p className={styles.planSelected}>
               {selectedPlan}
-              <HiChevronDown size={18} style={{transform: selectionContainer ? 'rotate(180deg)' : 'rotate(0deg)', transition: '400ms'}} />
+              <HiChevronDown
+                size={18}
+                style={{
+                  transform: selectionContainer
+                    ? "rotate(180deg)"
+                    : "rotate(0deg)",
+                  transition: "400ms",
+                }}
+              />
             </p>
           </div>
           <div
@@ -129,31 +178,49 @@ const SignupForm = ({accountInfo, setAccountInfo}) => {
                 : { opacity: 0, height: 0 }
             }
           >
-            <p
-              className={styles.selection}
-              onClick={() => setPlan(1)}
-            >
-              <BsCheckLg color="var(--gold)" style={{display: plan === 1 ? 'block' : 'none', marginRight: 10}} size={12} /> Free $0.00 /
-              month
+            <p className={styles.selection} onClick={() => setPlan(1)}>
+              <BsCheckLg
+                color="var(--gold)"
+                style={{
+                  display: plan === 1 ? "block" : "none",
+                  marginRight: 10,
+                }}
+                size={12}
+              />{" "}
+              Free $0.00 / month
             </p>
-            <p
-              className={styles.selection}
-              onClick={() => setPlan(2)}
-            >
-              <BsCheckLg color="var(--gold)" size={12} style={{display: plan === 2 ? 'block' : 'none', marginRight: 10}} /> Standard $8.99
-              / month
+            <p className={styles.selection} onClick={() => setPlan(2)}>
+              <BsCheckLg
+                color="var(--gold)"
+                size={12}
+                style={{
+                  display: plan === 2 ? "block" : "none",
+                  marginRight: 10,
+                }}
+              />{" "}
+              Standard $8.99 / month
             </p>
-            <p
-              className={styles.selection}
-              onClick={() => setPlan(3)}
-            >
-              <BsCheckLg color="var(--gold)" size={12} style={{display: plan === 3 ? 'block' : 'none', marginRight: 10}} /> Premium $14.99
-              / month
+            <p className={styles.selection} onClick={() => setPlan(3)}>
+              <BsCheckLg
+                color="var(--gold)"
+                size={12}
+                style={{
+                  display: plan === 3 ? "block" : "none",
+                  marginRight: 10,
+                }}
+              />{" "}
+              Premium $14.99 / month
             </p>
           </div>
         </div>
         <div className={styles.btnContainer}>
-          <button type="submit" className={styles.submitBtn}>{isLoading ? <ButtonLoader /> : buttonContent}</button>
+          <button
+            disabled={isLoading}
+            type="submit"
+            className={styles.submitBtn}
+          >
+            {isLoading ? <ButtonLoader /> : buttonContent}
+          </button>
         </div>
       </form>
     </div>
